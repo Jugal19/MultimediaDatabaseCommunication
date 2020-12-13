d8fe839fdbedf6ed6b22cb3262cdd287ee6a4397
Jugal Patel <jugalpatel@hotmail.ca>
2020-12-12 20:33:51 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MultimediaDatabaseCommunication.Models;
+namespace MultimediaDatabaseCommunication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
